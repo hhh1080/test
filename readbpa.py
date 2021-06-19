433e529093ec7a1cb049106911299332a3c97633
hhh1080 <2395627283@qq.com>
2021-06-19 11:52:02 +0800

    6.19 read 500KV lines data

--- readbpa.py
+++ readbpa.py
@@ -13,29 +13,6 @@ def readBPA():
     key1 = 'L ';key2 = 'T '
     #找到头两个字符为L（即线路卡片）的数据行，方法是将每一行的字符编码为utf-8。再以同样格式解码就可以提取
     #字符串中的任意一个字符
-    # print(lines[46126])
-    # print(lines[337])
-    # c=lines[46126].encode('utf-8')
-    # cc = lines[337].encode('utf-8')
-    # # byte=bytearray(c)
-    # byte=c
-    # bytecc=cc
-    # print('字节数组c')
-    # print(byte)
-    # print('字节数组d')
-    # print(bytecc)
-    # byte1=byte[14:18]
-    # byte2=bytecc[14:18]
-    # print(byte1)
-    # print(byte2)
-    # # d=c.decode('utf-8')
-    # # print(c)
-    # # print(d)
-    # a=lines[337].encode('gbk')
-    # byted=a[14:18]
-    # print(byted)
-    # print(byted.decode('gbk'))
-    # print(a)
     for i in range(0,len(lines)):
         a=lines[i].encode('utf-8')
         #这里需要非常注意的是：因为这个[0:2]本意是想获取每一行中头两个字符，并且该行字符已经暂时被编码为utf-8的格式，
@@ -54,10 +31,6 @@ def readBPA():
         elif c == key2:
             Lnum.append(int(i))
             Tdata.append(lines[i])
-    # for num in Ldata:
-    #     print(num)
-    # print(len(Ldata))
-    # print(len(Tdata))
     V500=[]
     print('show 500KV')
     for i in range(0,len(Ldata)):
@@ -68,10 +41,7 @@ def readBPA():
         print(y)
         print(i)
         print(Ldata[i])
-        # print(type(y))
         if float(y) >= 500:
             V500.append(Ldata[i])
-
-
 
 readBPA()
