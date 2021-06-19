1a39a75657700ba839edf5386339bfc982f8c29d
hhh1080 <2395627283@qq.com>
2021-06-19 11:43:06 +0800

    6.19 read 500KV lines data

--- readbpa.py
+++ readbpa.py
@@ -13,32 +13,65 @@ def readBPA():
     key1 = 'L ';key2 = 'T '
     #找到头两个字符为L（即线路卡片）的数据行，方法是将每一行的字符编码为utf-8。再以同样格式解码就可以提取
     #字符串中的任意一个字符
-    print(lines[46126])
-    c=lines[46126].encode('utf-8')
-    d=c.decode('utf-8')
-    print(c)
-    print(d)
+    # print(lines[46126])
+    # print(lines[337])
+    # c=lines[46126].encode('utf-8')
+    # cc = lines[337].encode('utf-8')
+    # # byte=bytearray(c)
+    # byte=c
+    # bytecc=cc
+    # print('字节数组c')
+    # print(byte)
+    # print('字节数组d')
+    # print(bytecc)
+    # byte1=byte[14:18]
+    # byte2=bytecc[14:18]
+    # print(byte1)
+    # print(byte2)
+    # # d=c.decode('utf-8')
+    # # print(c)
+    # # print(d)
+    # a=lines[337].encode('gbk')
+    # byted=a[14:18]
+    # print(byted)
+    # print(byted.decode('gbk'))
+    # print(a)
     for i in range(0,len(lines)):
         a=lines[i].encode('utf-8')
-        b=a.decode('utf-8')
-        if b[0:2] == key1 :
+        #这里需要非常注意的是：因为这个[0:2]本意是想获取每一行中头两个字符，并且该行字符已经暂时被编码为utf-8的格式，
+        #如果这两个字节没有中文，那么一个字节就可以代表一个中文，如果含有中文，那么这句代码就只截取了这个中文的utf-8编码
+        #的其中一个或两个字节，那么这样是不完整的，在decode中程序是无法解码这个字节，因此会报错
+        b=a[0:2]
+        check=a[0:1]
+        # print(b)
+        checkstr = check.decode('utf-8')
+        if checkstr == '.':
+            continue
+        c=b.decode('utf-8')
+        if c == key1 :
             Lnum.append(int(i))
             Ldata.append(lines[i])
-        elif b[0:2] == key2:
+        elif c == key2:
             Lnum.append(int(i))
             Tdata.append(lines[i])
-    # for num in Lnum:
+    # for num in Ldata:
     #     print(num)
     # print(len(Ldata))
     # print(len(Tdata))
     V500=[]
+    print('show 500KV')
     for i in range(0,len(Ldata)):
-        a = Ldata[i].encode('utf-8')
-        b = a.decode('utf-8')
-        # print(b[14:18])
-    #     vdegree = float(b[14:18])
-    #     if vdegree >= 500:
-    #         V500.append(Ldata[i])
-    # print(len(V500))
+        #有一些字符gbk是认不出来的，如果报错设置为忽略
+        x = Ldata[i].encode('gbk',errors='ignore')
+        vdegree = x[14:18]
+        y = vdegree.decode('gbk')
+        print(y)
+        print(i)
+        print(Ldata[i])
+        # print(type(y))
+        if float(y) >= 500:
+            V500.append(Ldata[i])
+
+
 
 readBPA()
---
+++ test_3.py
@@ -0,0 +1,5 @@
+
+a=b'\ue0e6'
+print(a.decode('unicode_escape'))
+# b='？'
+# print(b.encode('unicode_escape'))
